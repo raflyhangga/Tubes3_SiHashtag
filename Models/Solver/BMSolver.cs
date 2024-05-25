e45fbdcb7e07be9211683ee6e43e1613062a977a
RayNoor0 <rayendraalthaf04@gmail.com>
2024-05-25 12:09:58 +0700

    Update BMSolver.cs

--- Models/Solver/BMSolver.cs
+++ Models/Solver/BMSolver.cs
@@ -1,8 +1,141 @@
+using System;
 using System.Collections.Generic;
 
 public class BMSolver : FingerSolver{
-
     protected override void ProcessCalculation(SidikJari sj, List<SidikJari> listSj, ref FingerSolution sol){
+        SidikJari result = null;
+        for(int i = 0; i < listSj.Count; i++) {
+            int res = BMSearch(sj.Ascii, listSj[i].Ascii);
+            if(res != -1) {
+                result = listSj[i];
+                break;
+            }
+        }
 
+        // Case not found
+        if(result == null) {
+            //// TODO: LevenshteinDistance
+
+            double percentage = 0;
+            for(int i = 0; i < listSj.Count; i++) {
+                LevenshteinDistance.Solve("", "");
+            }
+            //// The above is unfinished yet
+            sol.PersentaseKecocokan = percentage;
+        } else {
+            sol.PersentaseKecocokan = 1;
+        }
+
+        sol.SidikJari = result;
+    }
+ 
+    // The preprocessing function for Boyer Moore's
+    // bad character heuristic
+    static void BadCharHeuristic(string pat, int[] badchar)
+    {
+        int size = pat.Length;
+        int i;
+ 
+        // Initialize all occurrences as -1
+        for (i = 0; i < 256; i++)
+            badchar[i] = -1;
+ 
+        // Fill the actual value of last occurrence
+        // of a character
+        for (i = 0; i < size; i++)
+            badchar[(int)pat[i]] = i;
+    }
+
+    static void GoodSuffixHeuristic(string pat, int[] goodSuffixShift)
+    {
+        int m = pat.Length;
+        int i, j;
+
+        // Initialize the suffixes array
+        int[] suffixes = new int[m-1];
+        suffixes[m - 1] = m;
+        for (i = m - 2, j = m - 1; i >= 0; i--)
+        {
+            while (j < m - 1 && pat[i] != pat[j])
+                j = suffixes[j + 1] - 1;
+            suffixes[i] = --j + 1;
+        }
+
+        // Initialize the goodSuffixShift array
+        for (i = 0; i < m; i++)
+            goodSuffixShift[i] = m;
+
+        // Populate the goodSuffixShift array
+        for (i = m - 1; i >= 0; i--)
+        {
+            if (suffixes[i] == i + 1)
+            {
+                for (j = 0; j < m - 1 - i; j++)
+                {
+                    if (goodSuffixShift[j] == m)
+                        goodSuffixShift[j] = m - 1 - i;
+                }
+            }
+        }
+
+        for (i = 0; i <= m - 2; i++)
+        {
+            goodSuffixShift[m - 1 - suffixes[i]] = m - 1 - i;
+        }
+    }
+ 
+    /* A pattern searching function that uses Bad
+    Character Heuristic of Boyer Moore Algorithm */
+    static int BMSearch(string txt, string pat)
+    {
+        int m = pat.Length;
+        int n = txt.Length;
+ 
+        int[] badchar = new int[256];
+        int[] goodSuffixShift = new int[256];
+ 
+        /* Fill the bad character array by calling
+            the preprocessing function badCharHeuristic()
+            for given pattern */
+        BadCharHeuristic(pat, badchar);
+        GoodSuffixHeuristic(pat, goodSuffixShift);
+ 
+        int s = 0; // s is shift of the pattern with
+                   // respect to text
+        while (s <= (n - m)) {
+            int j = m - 1;
+ 
+            /* Keep reducing index j of pattern while
+                characters of pattern and text are
+                matching at this shift s */
+            while (j >= 0 && pat[j] == txt[s + j])
+                j--;
+ 
+            /* If the pattern is present at current
+                shift, then index j will become -1 after
+                the above loop */
+            if (j < 0) {
+                return s;
+ 
+                /* Shift the pattern so that the next
+                    character in text aligns with the last
+                    occurrence of it in pattern.
+                    The condition s+m < n is necessary for
+                    the case when pattern occurs at the end
+                    of text */
+            }
+ 
+            else
+                /* Shift the pattern so that the bad
+                   character in text aligns with the last
+                   occurrence of it in pattern. The max
+                   function is used to make sure that we get
+                   a positive shift. We may get a negative
+                   shift if the last occurrence of bad
+                   character in pattern is on the right side
+                   of the current character. */
+                s += (goodSuffixShift[j] > j - badchar[txt[s+j]])? goodSuffixShift[j] :  j - badchar[txt[s+j]];
+        }
+        return -1;
     }
 }
