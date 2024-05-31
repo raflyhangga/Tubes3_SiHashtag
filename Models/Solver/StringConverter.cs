5e6428328ee3213f99c2469eea7b7f375bed4349
RayNoor0 <rayendraalthaf04@gmail.com>
2024-05-31 16:17:38 +0700

    fix: string alay convert

--- Models/Solver/StringConverter.cs
+++ Models/Solver/StringConverter.cs
@@ -8,37 +8,18 @@ public class StringConverter
     private static Dictionary<string, string> _replacement_map = new Dictionary<string, string>
         {
             {"g", "6"},
-            {"e", "3"},
-            {"i", "1"},
-            {"o", "0"},
             {"s", "5"},
             {"z", "2"},
-            {"a", "4"},
+            {"[aeiou4013]", string.Empty},
             {@"\s+", String.Empty},
-            {"[aeiou]", string.Empty},
-            
         };
     public static string StringConvert(string input)
     {
         // Convert all uppercase to lowercase
-        string lowerCaseString = input.ToLower();
+        string convertedString = input.ToLower();
 
-        // Delete all vowels
-        string noVowelsString = Regex.Replace(lowerCaseString, "[aeiou]", string.Empty);
-
-        // Delete all whitespace
-        string finalString = Regex.Replace(noVowelsString, @"\s+", string.Empty);
-
-        return finalString;
-    }
-
-    public static string AlayConvert(string input)
-    {
-        // Convert all uppercase to lowercase
-        string convertedString = input;
-        foreach (var dic in _replacement_map)
-        {
-            Regex.Replace(convertedString, dic.Key, dic.Value);
+        foreach(var dic in _replacement_map){
+            convertedString = Regex.Replace(convertedString, dic.Key, dic.Value);
         }
 
         return convertedString;
