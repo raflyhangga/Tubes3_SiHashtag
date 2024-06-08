41ad844d1e818c09976fb593e162aaae049d7160
RayNoor0 <rayendraalthaf04@gmail.com>
2024-06-08 11:28:27 +0700

    fix:  bm solver

--- Models/Solver/BMSolver.cs
+++ Models/Solver/BMSolver.cs
@@ -11,90 +11,44 @@ public class BMSolver : FingerSolver{
                 break;
             }
         }
-
-        // Case not found
-        if(result == null) {
-            //// TODO: LevenshteinDistance
-
-            double percentage = 0;
-            for(int i = 0; i < listSj.Count; i++) {
-                LevenshteinDistance.Solve("", "");
-            }
-            //// The above is unfinished yet
-            sol.PersentaseKecocokan = percentage;
-        } else {
-            sol.PersentaseKecocokan = 1;
-        }
-
         sol.SidikJari = result;
     }
  
-    // The preprocessing function for Boyer Moore's
-    // bad character heuristic
-    static void BadCharHeuristic(string pat, int[] badchar)
+    // last Occurence table preprocessing
+    static void LastOccurenceCalculation(string pat, int[] lastOc)
     {
         int size = pat.Length;
-        int i;
- 
-        // Initialize all occurrences as -1
-        for (i = 0; i < 256; i++)
-            badchar[i] = -1;
- 
-        // Fill the actual value of last occurrence
-        // of a character
-        for (i = 0; i < size; i++)
-            badchar[(int)pat[i]] = i;
+        for (int i = 0; i < 256; i++)
+            lastOc[i] = -1;
+        for (int i = 0; i < size; i++)
+            lastOc[(int)pat[i]] = i;
     }
- 
-    /* A pattern searching function that uses Bad
-    Character Heuristic of Boyer Moore Algorithm */
+
     static int BMSearch(string txt, string pat)
     {
-        int m = pat.Length;
         int n = txt.Length;
- 
-        int[] badchar = new int[256];
- 
-        /* Fill the bad character array by calling
-            the preprocessing function badCharHeuristic()
-            for given pattern */
-        BadCharHeuristic(pat, badchar);
- 
-        int s = 0; // s is shift of the pattern with
-                   // respect to text
-        while (s <= (n - m)) {
+        int m = pat.Length;
+
+        // preprocessing pattern
+        int[] lastOc = new int[256];
+        LastOccurenceCalculation(pat, lastOc);
+
+        int i = m - 1;
+        while (i <= (n - m)) {
             int j = m - 1;
- 
-            /* Keep reducing index j of pattern while
-                characters of pattern and text are
-                matching at this shift s */
-            while (j >= 0 && pat[j] == txt[s + j])
-                j--;
- 
-            /* If the pattern is present at current
-                shift, then index j will become -1 after
-                the above loop */
-            if (j < 0) {
-                return s;
- 
-                /* Shift the pattern so that the next
-                    character in text aligns with the last
-                    occurrence of it in pattern.
-                    The condition s+m < n is necessary for
-                    the case when pattern occurs at the end
-                    of text */
+
+            if(txt[i] == pat[j]){
+                if(j == 0)
+                    return i;
+                else{
+                    i--;
+                    j--;
+                }
+            } else{
+                // lookup to last occurence table
+                i = i + m - int.Min(j, 1 + lastOc[(int)txt[i]]); 
+                j = m - 1;
             }
- 
-            else
-                /* Shift the pattern so that the bad
-                   character in text aligns with the last
-                   occurrence of it in pattern. The max
-                   function is used to make sure that we get
-                   a positive shift. We may get a negative
-                   shift if the last occurrence of bad
-                   character in pattern is on the right side
-                   of the current character. */
-                s += (1 > j - badchar[txt[s+j]])? 1 :  j - badchar[txt[s+j]];
         }
         return -1;
     }
